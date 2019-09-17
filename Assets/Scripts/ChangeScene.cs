9a90e57cb056121fa2e2c7845d7740425d400e11
kirakira0 <kiratoal0@gmail.com>
2019-09-16 20:09:45 -0700

    Initial commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public Image blackImage;
+    public string nextScene;
+    float alpha;
+    
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        alpha = blackImage.color.a;
+        Debug.Log(alpha);
+
+        if (alpha == 1f)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+
+    public Camera mycam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(mycam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane)), Vector3.up);
+
+
+
+    }
+}
